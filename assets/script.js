5ccb2248def2f18dbf0c1e7ffdd5c1865b91380c
sjohn294 <johnson.sam26.sj@gmail.com>
2024-01-05 16:43:59 -0500

    if statements

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,33 @@ var losses = 0;
 
 var options = ["ROCK", "PAPER", "SCISSORS"];
 
-var playGame = function() {
-  var userChoice = window.prompt("enter Rock, Paper, or Scissors");
-  
-  if (!userChoice) {
-    return
-  }
+var playGame = function () {
+    var userChoice = window.prompt("enter Rock, Paper, or Scissors");
+
+    if (!userChoice) {
+        return
+    }
+
+    userChoice = userChoice.toUpperCase();
+
+    var index = Math.floor(Math.random() * options.length);
+    var computerChoice = options[index];
+
+    window.alert("The computer chose " + computerChoice);
+
+    if (userChoice == computerChoice) {
+        ties++;
+        window.alert("It Was A Tie")
+
+    } else if (
+    (userChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (userChoice === "PAPER" && computerChoice === "ROCK") ||
+    (userChoice === "SCISSORS" && computerChoice === "PAPER")
+
+) {
+wins++;
+window.alert("You Win!");
+}
 };
 
 playGame();
